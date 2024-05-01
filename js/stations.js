0c3fb872dba5c6d457043cbfeab9b5b22ba65e2e
JustinQuekZJ <jq2346@nyu.edu>
2024-04-30 23:41:25 -0400

    Initial commit

---
+++ js/stations.js
@@ -0,0 +1,704 @@
+var stationData = [
+    {
+      "PrecinctNum": 47,
+      "Precinct": "47th",
+      "Name": "NYPD 47th Precinct Station",
+      "Long-Lat": "-73.84750, 40.88750",
+      "Longitude": -73.8475,
+      "Latitude": 40.8875,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 52,
+      "Precinct": "52nd",
+      "Name": "NYPD 52nd Precinct Station",
+      "Long-Lat": "-73.87964, 40.86908",
+      "Longitude": -73.87964,
+      "Latitude": 40.86908,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 49,
+      "Precinct": "49th",
+      "Name": "NYPD 49th Precinct Station",
+      "Long-Lat": "-73.84430, 40.85611",
+      "Longitude": -73.8443,
+      "Latitude": 40.85611,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 46,
+      "Precinct": "46th",
+      "Name": "NYPD 46th Precinct Station",
+      "Long-Lat": "-73.90011, 40.85404",
+      "Longitude": -73.90011,
+      "Latitude": 40.85404,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 50,
+      "Precinct": "50th",
+      "Name": "NYPD 50th Precinct Station",
+      "Long-Lat": "-73.90237, 40.88350",
+      "Longitude": -73.90237,
+      "Latitude": 40.8835,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 48,
+      "Precinct": "48th",
+      "Name": "NYPD 48th Precinct Station",
+      "Long-Lat": "-73.90029, 40.84397",
+      "Longitude": -73.90029,
+      "Latitude": 40.84397,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 44,
+      "Precinct": "44th",
+      "Name": "NYPD 44th Precinct Station",
+      "Long-Lat": "-73.91951, 40.83762",
+      "Longitude": -73.91951,
+      "Latitude": 40.83762,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 42,
+      "Precinct": "42nd",
+      "Name": "NYPD 42nd Precinct Station",
+      "Long-Lat": "-73.91131, 40.82244",
+      "Longitude": -73.91131,
+      "Latitude": 40.82244,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 45,
+      "Precinct": "45th",
+      "Name": "NYPD 45th Precinct Station",
+      "Long-Lat": "-73.82732, 40.83084",
+      "Longitude": -73.82732,
+      "Latitude": 40.83084,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 40,
+      "Precinct": "40th",
+      "Name": "NYPD 40th Precinct Station",
+      "Long-Lat": "-73.92522, 40.81033",
+      "Longitude": -73.92522,
+      "Latitude": 40.81033,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 41,
+      "Precinct": "41st",
+      "Name": "NYPD 41st Precinct Station",
+      "Long-Lat": "-73.89551, 40.81619",
+      "Longitude": -73.89551,
+      "Latitude": 40.81619,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 43,
+      "Precinct": "43rd",
+      "Name": "NYPD 43rd Precinct Station",
+      "Long-Lat": "-73.86981, 40.82288",
+      "Longitude": -73.86981,
+      "Latitude": 40.82288,
+      "Borough": "Bronx"
+    },
+    {
+      "PrecinctNum": 26,
+      "Precinct": "26th",
+      "Name": "NYPD 26th Precinct Station",
+      "Long-Lat": "-73.95653, 40.81469",
+      "Longitude": -73.95653,
+      "Latitude": 40.81469,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 24,
+      "Precinct": "24th",
+      "Name": "NYPD 24th Precinct Station",
+      "Long-Lat": "-73.96736, 40.79640",
+      "Longitude": -73.96736,
+      "Latitude": 40.7964,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 23,
+      "Precinct": "23rd",
+      "Name": "NYPD 23rd Precinct Station",
+      "Long-Lat": "-73.94702, 40.78908",
+      "Longitude": -73.94702,
+      "Latitude": 40.78908,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": "Central Park",
+      "Precinct": "Central Park",
+      "Name": "NYPD Central Park Precinct Station",
+      "Long-Lat": "-73.96415, 40.78314",
+      "Longitude": -73.96415,
+      "Latitude": 40.78314,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 20,
+      "Precinct": "20th",
+      "Name": "NYPD 20th Precinct Station",
+      "Long-Lat": "-73.97508, 40.78412",
+      "Longitude": -73.97508,
+      "Latitude": 40.78412,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 19,
+      "Precinct": "19th",
+      "Name": "NYPD 19th Precinct Station",
+      "Long-Lat": "-73.96387, 40.76707",
+      "Longitude": -73.96387,
+      "Latitude": 40.76707,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 18,
+      "Precinct": "Midtown North",
+      "Name": "NYPD Midtown North (18th) Precinct Station",
+      "Long-Lat": "-73.98508, 40.76494",
+      "Longitude": -73.98508,
+      "Latitude": 40.76494,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 17,
+      "Precinct": "17th",
+      "Name": "NYPD 17th Precinct Station",
+      "Long-Lat": "-73.97079, 40.75676",
+      "Longitude": -73.97079,
+      "Latitude": 40.75676,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 14,
+      "Precinct": "Midtown South",
+      "Name": "NYPD Midtown South (14th) Precinct Station",
+      "Long-Lat": "-73.99494, 40.75390",
+      "Longitude": -73.99494,
+      "Latitude": 40.7539,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 10,
+      "Precinct": "10th",
+      "Name": "NYPD 10th Precinct Station",
+      "Long-Lat": "-73.99865, 40.74269",
+      "Longitude": -73.99865,
+      "Latitude": 40.74269,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 13,
+      "Precinct": "13th",
+      "Name": "NYPD 13th Precinct Station",
+      "Long-Lat": "-73.98280, 40.73685",
+      "Longitude": -73.9828,
+      "Latitude": 40.73685,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 6,
+      "Precinct": "6th",
+      "Name": "NYPD 6th Precinct Station",
+      "Long-Lat": "-74.00545, 40.73404",
+      "Longitude": -74.00545,
+      "Latitude": 40.73404,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 9,
+      "Precinct": "9th",
+      "Name": "NYPD 9th Precinct Station",
+      "Long-Lat": "-73.98783, 40.72654",
+      "Longitude": -73.98783,
+      "Latitude": 40.72654,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 1,
+      "Precinct": "1st",
+      "Name": "NYPD 1st Precinct Station",
+      "Long-Lat": "-74.00704, 40.72035",
+      "Longitude": -74.00704,
+      "Latitude": 40.72035,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 7,
+      "Precinct": "7th",
+      "Name": "NYPD 7th Precinct Station",
+      "Long-Lat": "-73.98372, 40.71636",
+      "Longitude": -73.98372,
+      "Latitude": 40.71636,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 5,
+      "Precinct": "5th",
+      "Name": "NYPD 5th Precinct Station",
+      "Long-Lat": "-73.99746, 40.71618",
+      "Longitude": -73.99746,
+      "Latitude": 40.71618,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 34,
+      "Precinct": "34th",
+      "Name": "NYPD 34th Precinct Station",
+      "Long-Lat": "-73.93537, 40.85153",
+      "Longitude": -73.93537,
+      "Latitude": 40.85153,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 33,
+      "Precinct": "33rd",
+      "Name": "NYPD 33rd Precinct Station",
+      "Long-Lat": "-73.93576, 40.84061",
+      "Longitude": -73.93576,
+      "Latitude": 40.84061,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 30,
+      "Precinct": "30th",
+      "Name": "NYPD 30th Precinct Station",
+      "Long-Lat": "-73.94375, 40.82882",
+      "Longitude": -73.94375,
+      "Latitude": 40.82882,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 32,
+      "Precinct": "32nd",
+      "Name": "NYPD 32nd Precinct Station",
+      "Long-Lat": "-73.94540, 40.81575",
+      "Longitude": -73.9454,
+      "Latitude": 40.81575,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 28,
+      "Precinct": "28th",
+      "Name": "NYPD 28th Precinct Station",
+      "Long-Lat": "-73.95259, 40.80883",
+      "Longitude": -73.95259,
+      "Latitude": 40.80883,
+      "Borough": "Manhattan"
+    },
+    {
+        "PrecinctNum": 25,
+        "Precinct": "25th",
+        "Name": "NYPD 25th Precinct Station",
+        "Long-Lat": "-73.94112, 40.80069",
+        "Longitude": -73.94112,
+        "Latitude": 40.80069,
+        "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 24,
+      "Precinct": "24th",
+      "Name": "NYPD 24th Precinct Station",
+      "Long-Lat": "-73.94112, 40.80069",
+      "Longitude": -73.94112,
+      "Latitude": 40.80069,
+      "Borough": "Manhattan"
+    },
+    {
+      "PrecinctNum": 108,
+      "Precinct": "108th",
+      "Name": "NYPD 108th Precinct Station",
+      "Long-Lat": "-73.95473, 40.74305",
+      "Longitude": -73.95473,
+      "Latitude": 40.74305,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 114,
+      "Precinct": "114th",
+      "Name": "NYPD 114th Precinct Station",
+      "Long-Lat": "-73.91533, 40.76930",
+      "Longitude": -73.91533,
+      "Latitude": 40.7693,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 115,
+      "Precinct": "115th",
+      "Name": "NYPD 115th Precinct Station",
+      "Long-Lat": "-73.87553, 40.75685",
+      "Longitude": -73.87553,
+      "Latitude": 40.75685,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 110,
+      "Precinct": "110th",
+      "Name": "NYPD 110th Precinct Station",
+      "Long-Lat": "-73.86994, 40.74524",
+      "Longitude": -73.86994,
+      "Latitude": 40.74524,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 104,
+      "Precinct": "104th",
+      "Name": "NYPD 104th Precinct Station",
+      "Long-Lat": "-73.89361, 40.70427",
+      "Longitude": -73.89361,
+      "Latitude": 40.70427,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 109,
+      "Precinct": "109th",
+      "Name": "NYPD 109th Precinct Station",
+      "Long-Lat": "-73.82700, 40.76236",
+      "Longitude": -73.827,
+      "Latitude": 40.76236,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 111,
+      "Precinct": "111th",
+      "Name": "NYPD 111th Precinct Station",
+      "Long-Lat": "-73.76770, 40.76028",
+      "Longitude": -73.7677,
+      "Latitude": 40.76028,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 105,
+      "Precinct": "105th",
+      "Name": "NYPD 105th Precinct Station",
+      "Long-Lat": "-73.73514, 40.72622",
+      "Longitude": -73.73514,
+      "Latitude": 40.72622,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 107,
+      "Precinct": "107th",
+      "Name": "NYPD 107th Precinct Station",
+      "Long-Lat": "-73.81027, 40.72995",
+      "Longitude": -73.81027,
+      "Latitude": 40.72995,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 103,
+      "Precinct": "103rd",
+      "Name": "NYPD 103rd Precinct Station",
+      "Long-Lat": "-73.79266, 40.70712",
+      "Longitude": -73.79266,
+      "Latitude": 40.70712,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 102,
+      "Precinct": "102nd",
+      "Name": "NYPD 102nd Precinct Station",
+      "Long-Lat": "-73.83145, 40.69872",
+      "Longitude": -73.83145,
+      "Latitude": 40.69872,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 106,
+      "Precinct": "106th",
+      "Name": "NYPD 106th Precinct Station",
+      "Long-Lat": "-73.83961, 40.68220",
+      "Longitude": -73.83961,
+      "Latitude": 40.6822,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 100,
+      "Precinct": "100th",
+      "Name": "NYPD 100th Precinct Station",
+      "Long-Lat": "-73.81648, 40.58625",
+      "Longitude": -73.81648,
+      "Latitude": 40.58625,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 101,
+      "Precinct": "101st",
+      "Name": "NYPD 101st Precinct Station",
+      "Long-Lat": "-73.75000, 40.60295",
+      "Longitude": -73.75,
+      "Latitude": 40.60295,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 112,
+      "Precinct": "112th",
+      "Name": "NYPD 112th Precinct Station",
+      "Long-Lat": "-73.85112, 40.72225",
+      "Longitude": -73.85112,
+      "Latitude": 40.72225,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 113,
+      "Precinct": "113th",
+      "Name": "NYPD 113th Precinct Station",
+      "Long-Lat": "-73.77528, 40.68013",
+      "Longitude": -73.77528,
+      "Latitude": 40.68013,
+      "Borough": "Queens"
+    },
+    {
+      "PrecinctNum": 94,
+      "Precinct": "94th",
+      "Name": "NYPD 94th Precinct Station",
+      "Long-Lat": "-73.95329, 40.72674",
+      "Longitude": -73.95329,
+      "Latitude": 40.72674,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 90,
+      "Precinct": "90th",
+      "Name": "NYPD 90th Precinct Station",
+      "Long-Lat": "-73.95058, 40.70641",
+      "Longitude": -73.95058,
+      "Latitude": 40.70641,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 84,
+      "Precinct": "84th",
+      "Name": "NYPD 84th Precinct Station",
+      "Long-Lat": "-73.98284, 40.69540",
+      "Longitude": -73.98284,
+      "Latitude": 40.6954,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 83,
+      "Precinct": "83rd",
+      "Name": "NYPD 83rd Precinct Station",
+      "Long-Lat": "-73.91787, 40.69818",
+      "Longitude": -73.91787,
+      "Latitude": 40.69818,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 76,
+      "Precinct": "76th",
+      "Name": "NYPD 76th Precinct Station",
+      "Long-Lat": "-74.00024, 40.68386",
+      "Longitude": -74.00024,
+      "Latitude": 40.68386,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 78,
+      "Precinct": "78th",
+      "Name": "NYPD 78th Precinct Station",
+      "Long-Lat": "-73.97413, 40.68094",
+      "Longitude": -73.97413,
+      "Latitude": 40.68094,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 88,
+      "Precinct": "88th",
+      "Name": "NYPD 88th Precinct Station",
+      "Long-Lat": "-73.96051, 40.69008",
+      "Longitude": -73.96051,
+      "Latitude": 40.69008,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 79,
+      "Precinct": "79th",
+      "Name": "NYPD 79th Precinct Station",
+      "Long-Lat": "-73.94465, 40.68891",
+      "Longitude": -73.94465,
+      "Latitude": 40.68891,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 81,
+      "Precinct": "81st",
+      "Name": "NYPD 81st Precinct Station",
+      "Long-Lat": "-73.92441, 40.68969",
+      "Longitude": -73.92441,
+      "Latitude": 40.68969,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 77,
+      "Precinct": "77th",
+      "Name": "NYPD 77th Precinct Station",
+      "Long-Lat": "-73.93025, 40.67471",
+      "Longitude": -73.93025,
+      "Latitude": 40.67471,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 73,
+      "Precinct": "73rd",
+      "Name": "NYPD 73rd Precinct Station",
+      "Long-Lat": "-73.91353, 40.67081",
+      "Longitude": -73.91353,
+      "Latitude": 40.67081,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 75,
+      "Precinct": "75th",
+      "Name": "NYPD 75th Precinct Station",
+      "Long-Lat": "-73.88151, 40.67115",
+      "Longitude": -73.88151,
+      "Latitude": 40.67115,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 71,
+      "Precinct": "71st",
+      "Name": "NYPD 71st Precinct Station",
+      "Long-Lat": "-73.94788, 40.66458",
+      "Longitude": -73.94788,
+      "Latitude": 40.66458,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 72,
+      "Precinct": "72nd",
+      "Name": "NYPD 72nd Precinct Station",
+      "Long-Lat": "-74.00105, 40.65819",
+      "Longitude": -74.00105,
+      "Latitude": 40.65819,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 69,
+      "Precinct": "69th",
+      "Name": "NYPD 69th Precinct Station",
+      "Long-Lat": "-73.90495, 40.64849",
+      "Longitude": -73.90495,
+      "Latitude": 40.64849,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 68,
+      "Precinct": "68th",
+      "Name": "NYPD 68th Precinct Station",
+      "Long-Lat": "-74.02256, 40.63903",
+      "Longitude": -74.02256,
+      "Latitude": 40.63903,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 70,
+      "Precinct": "70th",
+      "Name": "NYPD 70th Precinct Station",
+      "Long-Lat": "-73.97372, 40.63036",
+      "Longitude": -73.97372,
+      "Latitude": 40.63036,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 63,
+      "Precinct": "63rd",
+      "Name": "NYPD 63rd Precinct Station",
+      "Long-Lat": "-73.94165, 40.62796",
+      "Longitude": -73.94165,
+      "Latitude": 40.62796,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 62,
+      "Precinct": "62nd",
+      "Name": "NYPD 62nd Precinct Station",
+      "Long-Lat": "-74.00269, 40.60234",
+      "Longitude": -74.00269,
+      "Latitude": 40.60234,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 66,
+      "Precinct": "66th",
+      "Name": "NYPD 66th Precinct Station",
+      "Long-Lat": "-73.99140, 40.62576",
+      "Longitude": -73.9914,
+      "Latitude": 40.62576,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 61,
+      "Precinct": "61st",
+      "Name": "NYPD 61st Precinct Station",
+      "Long-Lat": "-73.96048, 40.59411",
+      "Longitude": -73.96048,
+      "Latitude": 40.59411,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 60,
+      "Precinct": "60th",
+      "Name": "NYPD 60th Precinct Station",
+      "Long-Lat": "-73.97613, 40.57654",
+      "Longitude": -73.97613,
+      "Latitude": 40.57654,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 67,
+      "Precinct": "67th",
+      "Name": "NYPD 67th Precinct Station",
+      "Long-Lat": "-73.95020, 40.64876",
+      "Longitude": -73.9502,
+      "Latitude": 40.64876,
+      "Borough": "Brooklyn"
+    },
+    {
+      "PrecinctNum": 120,
+      "Precinct": "120th",
+      "Name": "NYPD 120th Precinct Station",
+      "Long-Lat": "-74.07745, 40.64464",
+      "Longitude": -74.07745,
+      "Latitude": 40.64464,
+      "Borough": "Staten Island"
+    },
+    {
+      "PrecinctNum": 121,
+      "Precinct": "121st",
+      "Name": "NYPD 121st Precinct Station",
+      "Long-Lat": "-74.15063, 40.62365",
+      "Longitude": -74.15063,
+      "Latitude": 40.62365,
+      "Borough": "Staten Island"
+    },
+    {
+      "PrecinctNum": 122,
+      "Precinct": "122nd",
+      "Name": "NYPD 122nd Precinct Station",
+      "Long-Lat": "-74.10553, 40.57413",
+      "Longitude": -74.10553,
+      "Latitude": 40.57413,
+      "Borough": "Staten Island"
+    },
+    {
+      "PrecinctNum": 123,
+      "Precinct": "123rd",
+      "Name": "NYPD 123rd Precinct Station",
+      "Long-Lat": "-74.24998, 40.51187",
+      "Longitude": -74.24998,
+      "Latitude": 40.51187,
+      "Borough": "Staten Island"
+    }
+  ]
